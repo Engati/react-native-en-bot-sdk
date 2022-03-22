66c7e70508d720d9e47ac20c3efe2be7cacd353d
NiteshGoyal <nitesh.goyal@gdn-commerce.com>
2022-03-22 12:19:06 +0530

    Added engati sdk and launched bot with hardcoded values

--- android/src/main/java/com/reactnativeenbotsdk/EnBotSdkModule.kt
+++ android/src/main/java/com/reactnativeenbotsdk/EnBotSdkModule.kt
@@ -4,6 +4,8 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
+import com.en.botsdk.Constants.LanguageAlignment
+import com.en.botsdk.ui.ChatBotConfig
 
 class EnBotSdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
@@ -15,10 +17,18 @@ class EnBotSdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJa
     // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
     fun multiply(a: Int, b: Int, promise: Promise) {
-    
+
       promise.resolve(a * b)
-    
+      ChatBotConfig.getInstance().init("7b2f8fe62c8d4176", "botName", true, "default",
+        getCurrentActivity(), "botUserId", 100, false, LanguageAlignment.DEFAULT,
+        null, null, R.drawable.icon_send, null)
+      ChatBotConfig.getInstance().launchBot()
     }
 
-    
+    @ReactMethod
+    fun launchBot(botKey: String, botName: String, welcomeMsg: Boolean) {
+
+    }
+
+
 }
