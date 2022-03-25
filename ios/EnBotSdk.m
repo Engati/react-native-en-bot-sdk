3ca5ac89672c2352f7190cbbf7e3d6bf749cf606
Mayur Kothawade <mayur.kothawade@pimonk.com>
2022-03-25 17:04:30 +0530

    React native SDK Interface pushed, iOS integration completed.

--- ios/EnBotSdk.m
+++ ios/EnBotSdk.m
@@ -5,5 +5,22 @@
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
+                 
+RCT_EXTERN_METHOD(launchBot:(NSString *)botKey
+                  botName:(NSString *)botName
+                  brandingKey:(NSString *)brandingKey
+                  welcomeMsg:(BOOL)welcomeMsg
+                  enableLog:(BOOL)enableLog
+                  userId:(NSString *)userId
+                  chatHistorySize:(int)chatHistorySize
+                  showDone:(BOOL)showDone
+                  closeButtonImage:(NSString *)closeButtonImage
+                  language:(int)language
+                  headerTitleFont:(NSString *)headerTitleFont
+                  headerDescriptionFont:(NSString *)headerDescriptionFont
+                  sendMessageButtonIcon:(NSString *)sendMessageButtonIcon
+                  userAttributes:(id)userAttributes
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
--- react-native-en-bot-sdk.podspec
+++ react-native-en-bot-sdk.podspec
@@ -10,10 +10,18 @@ Pod::Spec.new do |s|
   s.license      = package["license"]
   s.authors      = package["author"]
 
-  s.platforms    = { :ios => "10.0" }
+  s.platforms    = { :ios => "12.0" }
   s.source       = { :git => "https://github.com/Engati/react-native-enbotsdk.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,mm,swift}"
-
+ 
   s.dependency "React-Core"
+  s.dependency "ENChatBot"
+
+  s.pod_target_xcconfig =
+  {
+     'BUILD_LIBRARY_FOR_DISTRIBUTION' => 'YES',
+     'IPHONEOS_DEPLOYMENT_TARGET' => '12.0',
+     'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'arm64'
+  }
 end
