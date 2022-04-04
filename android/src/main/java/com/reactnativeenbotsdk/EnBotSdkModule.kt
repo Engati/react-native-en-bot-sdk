d196d461213563e60932745446ccad38b36355c6
NiteshGoyal <nitesh.goyal@gdn-commerce.com>
2022-04-04 08:04:21 +0530

    added support for send button image and done button callback

--- android/src/main/java/com/reactnativeenbotsdk/EnBotSdkModule.kt
+++ android/src/main/java/com/reactnativeenbotsdk/EnBotSdkModule.kt
@@ -2,36 +2,39 @@ package com.reactnativeenbotsdk
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Typeface
+import android.util.Log
 import com.en.botsdk.Constants.LanguageAlignment
 import com.en.botsdk.ui.ChatBotConfig
-import com.facebook.react.bridge.Callback
+import com.facebook.react.bridge.*
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.bridge.*
 
 class EnBotSdkModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
-//  private var myFailureCallback: Callback? = null
-//  private var mySuccessCallback: Callback? = null
-//  private val activityEventListener =
-//    object : BaseActivityEventListener() {
-//      override fun onActivityResult(
-//        activity: Activity?,
-//        requestCode: Int,
-//        resultCode: Int,
-//        intent: Intent?
-//      ) {
-//
-//        print(requestCode)
-//      }
-//    }
-//  init {
-//    reactContext.addActivityEventListener(activityEventListener)
-//  }
+  private var promise: Promise? = null
+  private val TAG = "EnBotSdkModule"
+  private val activityEventListener =
+    object : BaseActivityEventListener() {
+      override fun onActivityResult(
+        activity: Activity?,
+        requestCode: Int,
+        resultCode: Int,
+        intent: Intent?
+      ) {
+        promise?.resolve(resultCode)
+        print(requestCode)
+      }
+    }
+
+  init {
+    reactContext.addActivityEventListener(activityEventListener)
+  }
 
   override fun getName(): String {
     return "EnBotSdk"
@@ -57,9 +60,9 @@ class EnBotSdkModule(reactContext: ReactApplicationContext) :
     welcomeMsg: Boolean, enableLog: Boolean, userId: String, chatHistorySize: Int,
     showDone: Boolean, closeButtonImage: String, language: Int,
     headerTitleFont: String, headerDescriptionFont: String,
-    sendMessageButtonIcon: String, userAttributes: ReadableMap
+    sendMessageButtonIcon: String, userAttributes: ReadableMap, promise: Promise
   ) {
-
+    this.promise = promise
     val languageAlignment =
       if (language == 1) LanguageAlignment.RTL else LanguageAlignment.DEFAULT
     val titleTypeface = Typeface.createFromAsset(
@@ -70,16 +73,30 @@ class EnBotSdkModule(reactContext: ReactApplicationContext) :
       getCurrentActivity()?.getAssets(),
       "fonts/$headerDescriptionFont"
     )
+
+    val sendBitmap = getSendBitmap(sendMessageButtonIcon)
+
     ChatBotConfig.getInstance().init(
       botKey, botName, welcomeMsg, brandingKey,
       getCurrentActivity(), userId, chatHistorySize, showDone, languageAlignment,
-      titleTypeface, descriptionTypeface, R.drawable.icon_send, null
+      titleTypeface, descriptionTypeface, R.drawable.icon_send, sendBitmap
     )
 //    this.myFailureCallback = myFailureCallback
 //    this.mySuccessCallback = mySuccessCallback
 //      ChatBotConfig.getInstance().setDataAttributes(userAttributes)
 
     ChatBotConfig.getInstance().launchBot(1234)
+  }
+
+  private fun getSendBitmap(url: String): Bitmap? {
+    var sendBitmap: Bitmap? = null
+    try {
+      val inputStream = getCurrentActivity()?.getAssets()?.open(url)
+      sendBitmap = BitmapFactory.decodeStream(inputStream)
+    } catch (e: Exception) {
+      Log.e(TAG, "Error: " + e.message)
+    }
+    return sendBitmap
   }
 
 }
